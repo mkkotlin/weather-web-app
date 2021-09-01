a966a1ddc214a82c15d7b9adb569b86e7ed732c0
Mayank Kumar <87866647+mkkotlin@users.noreply.github.com>
2021-09-02 02:50:28 +0530

    Add files via upload

---
+++ weatherapp/request.js
@@ -0,0 +1,15 @@
+const key = 'aaeeccb8a4f15b1ade59473d076d57c9';
+
+
+const requestCity = async (city) => {
+    const baseURL = 'http://api.openweathermap.org/data/2.5/weather'
+    const query = `?q=${city}&appid=${key}`;
+
+    //make fetch call (promise call)
+    const response = await fetch(baseURL + query);
+
+    //promise data
+    const data = await response.json();
+    return data;
+
+}
